e67b2c6646c5a44acbc4be1bd846c85c72cd0695
Awashcard0 <95319856+Awashcard0@users.noreply.github.com>
2022-11-18 11:42:20 -0500

    yee

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+var app = document.getElementById('app');
 
+var typewriter = new Typewriter(app, {
+  loop: true
+});
+
+typewriter.typeString('Hello')
+  .pauseFor(2500)
+  .deleteAll()
+  .typeString('The time is ti:me')
+  .pauseFor(2500)
+  .deleteChars(13)
+  .typeString("date is DD/MM/YYYY")
+  //.typeString('<strong></strong>')
+  .pauseFor(2500)
+  .start();
